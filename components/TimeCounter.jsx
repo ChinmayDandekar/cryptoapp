ca8cfcd8c257e4b5f7e16d1a27bc04884ba10a5f
Chinmay Dandekar <chinmay2001.cd@gmail.com>
2023-12-08 11:55:40 +0530

    Added Responsiveness

--- components/TimeCounter.jsx
+++ components/TimeCounter.jsx
@@ -6,7 +6,7 @@ const TimeCounter = () => {
   return (
     <div className='flex flex-row w-auto overflow-visible items-center relative'>
             <div className='flex flex-row bg-light w-auto h-10 mr-10 pr-10 pl-4 lg:pr-5 lg:pl-2 lg:h-8 items-center rounded-full ' >
-                <div className=' font-black text-purple text-xs '>00:33</div>
+                <div className=' font-black text-purple lg:text-xs '>00:33</div>
                 <div className='text-[12px] px-2 font-medium text-textcol lg:text-[10px]'>5m</div>
 
             </div>
